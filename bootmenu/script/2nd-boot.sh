c89981fed3b329e19cc61c2f73ddda906f50dcd2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-23 08:02:55 +0200

    update 2nd-boot profile with jb early builds
    replace 2nd-boot init by 4.1.1 jb one...
    
    Change-Id: I9d3284a38c229b80ac25dbbc4d984986a162a335

--- bootmenu/script/2nd-boot.sh
+++ bootmenu/script/2nd-boot.sh
@@ -1,21 +1,23 @@
-#!/sbin/sh
+#!/sbin/busybox ash
 
 ######## BootMenu Script
 ######## Execute [2nd-boot] Menu
-
-export PATH=/sbin:/system/xbin:/system/bin
 
 source /system/bootmenu/script/_config.sh
 
 ######## Main Script
 
-mount -o remount,rw /
+toolbox mount -o remount,rw rootfs /
 rm -f /*.rc
+rm -f /*.sh
 rm -f /osh
 rm -rf /preinstall
-cp -r -f /system/bootmenu/2nd-boot/* /
+cp -f /system/bootmenu/2nd-boot/* /
 chmod 640 /*.rc
 chmod 750 /init
+rm -f /sbin/ueventd
+ln -s /init /sbin/ueventd
+cp -f /system/bootmenu/binary/adbd /sbin/adbd
 killall ueventd
 
 ADBD_RUNNING=`ps | grep adbd | grep -v grep`
@@ -25,13 +27,16 @@ fi
 
 # original /tmp data symlink
 if [ -L /tmp.bak ]; then
-  rm /tmp.bak
+    rm /tmp.bak
 fi
 
 if [ -L /sdcard-ext ]; then
     rm /sdcard-ext
     mkdir -p /sd-ext
 fi
+
+busybox mkdir -p /storage
+ln -s /mnt/sdcard /storage/sdcard0
 
 ## unmount devices
 sync
@@ -41,13 +46,14 @@ umount /dev/pts
 umount /mnt/asec
 umount /mnt/obb
 umount /cache
-umount /data/tmp
 umount /data
-mount -o remount,rw,relatime,mode=775,size=128k /dev
+
+## adbd shell
+ln -s /system/xbin/bash /sbin/sh
 
 ######## Cleanup
 
-rm /sbin/lsof
+rm -f /sbin/lsof
 
 ## busybox cleanup..
 if [ -f /sbin/busybox ]; then
@@ -58,12 +64,8 @@ if [ -f /sbin/busybox ]; then
     rm -f /sbin/busybox
 fi
 
-## adbd shell
-ln -s /system/xbin/bash /sbin/sh
-
 ## reduce lcd backlight to save battery
 echo 18 > /sys/class/leds/lcd-backlight/brightness
-
 
 ######## Let's go
 
