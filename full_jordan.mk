db2734e1d937021a46c41715627073c3bd0e76ff
Quarx2k <agent00791@gmail.com>
2012-07-24 17:37:14 +0700

    use common vendor

--- full_jordan.mk
+++ full_jordan.mk
@@ -25,7 +25,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/moto/jordan-common/device.mk)
 # Also get non-open-source files if available
-#$(call inherit-product, vendor/motorola/jordan_common/jordan-vendor.mk)
+$(call inherit-product, vendor/motorola/jordan-common/jordan-vendor.mk)
 
 PRODUCT_NAME := full_jordan
 PRODUCT_DEVICE := jordan
