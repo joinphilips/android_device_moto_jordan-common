e5d7f025579b3cd814d7b12e3276fde861e6d8da
Quarx2k <agent00791@gmail.com>
2012-07-25 16:36:21 +0700

    allow use by non-jordan devices (audio policy)
    Change-Id: I434f247c55a698ef41bb16c9b6c5f9bf9f13d502

--- audio_policy/Android.mk
+++ audio_policy/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),jordan)
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -9,7 +7,7 @@ LOCAL_SHARED_LIBRARIES:= libc libcutils libutils libmedia
 LOCAL_STATIC_LIBRARIES := libmedia_helper
 LOCAL_WHOLE_STATIC_LIBRARIES:= libaudiopolicy_legacy
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE:= audio_policy.jordan
+LOCAL_MODULE:= audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_TAGS := optional
 
 ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
@@ -27,7 +25,7 @@ ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := audio.primary.jordan
+LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := \
@@ -43,7 +41,4 @@ LOCAL_WHOLE_STATIC_LIBRARIES := \
     libaudiohw_legacy
 include $(BUILD_SHARED_LIBRARY)
 
-endif #BOARD_USES_AUDIO_LEGACY
-
-endif # jordan
-
+endif
